abf6e856e29466bbe1b04b40992c236eadf5ee50
agismlna2 <160321038+agismlna2@users.noreply.github.com>
2024-02-21 08:35:01 +0700

    Add files via upload

---
+++ mykasir/hapus/hapus-produk.php
@@ -0,0 +1,25 @@
+<?php
+
+include "../config.php";
+
+if( isset($_GET['id_produk']) ){
+
+    // ambil id dari query string
+    $id_produk = $_GET['id_produk'];
+
+    // buat query hapus
+    $sql = "DELETE FROM produk WHERE id_produk=$id_produk";
+    $query = mysqli_query($conn, $sql);
+
+    // apakah query hapus berhasil?
+    if( $query ){
+        header("Location: ../produk/produk.php?status=sukses");
+    } else {
+        die("gagal menghapus...");
+    }
+
+} else {
+    die("akses dilarang...");
+}
+
+?>
