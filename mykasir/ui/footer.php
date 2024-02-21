8f9192ac16b5316b9f6084517a8d50b4f8998cc8
agismlna2 <160321038+agismlna2@users.noreply.github.com>
2024-02-21 08:47:25 +0700

    Add files via upload

---
+++ mykasir/ui/footer.php
@@ -0,0 +1,7 @@
+
+
+    <footer class="footer mt-auto py-3 bg-light">
+      <div class="container-fluid text-center">
+        <span class="text-muted">&copy; Agis Maulana UKK 2024</span>
+      </div>
+    </footer>
---
+++ mykasir/ui/nav-bar.php
@@ -0,0 +1,31 @@
+
+
+
+
+ <div class="container mt-3 shadow p-3 mb-3 bg-body-tertiary rounded">
+<ul class="nav nav-pills nav-fill gap-2 p-1 small bg-light rounded-5 shadow-lg w-100 h-25" id="pillNav2" role="tablist" style="--bs-nav-link-color: var(--bs-black); --bs-nav-pills-link-active-color: var(--bs-light); --bs-nav-pills-link-active-bg: var(--bs-white);">
+  <li class="nav-item">
+    <a class="btn btn-info" href="../mykasir/index.php">Home</a>
+  </li>
+  
+  <li class="nav-item">
+    <a class="btn btn-info" href="../mykasir/produk/produk.php">Produk</a>
+  </li>
+  
+  <li class="nav-item">
+    <a class="btn btn-info"  href="../mykasir/penjualan/penjualan.php">Penjualan</a>
+  </li>
+  
+  <li class="nav-item">
+    <a class="btn btn-info" href="../mykasir/pelanggan/input-pelanggan.php">Pelanggan</a>
+  </li>
+  
+  
+  <li class="nav-item">
+    <a class="btn btn-info"  href="../mykasir/regis/registrasi.php">Karyawan</a>
+  </li>
+  
+  <li class="nav-item">
+   <a class="btn btn-danger" href="../mykasir/logout.php">Logout</a>
+  </li>
+</ul>
